45a650b3b8b04d989af0d2aa7e6f87e284e559a9
Andrei Lysenko <andrei.lysenko@outlook.com>
2020-04-18 03:12:43 -0700

    MVP

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, withRouter } from 'react-router-dom';
 import './sass/index.scss';
 import App from './App';
+import { ProductContext } from "./contexts/ProductContext";
 
 const AppWithRouter = withRouter(App);
 
